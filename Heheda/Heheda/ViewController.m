43ffa3bcb3bc1f497b15adcdd627ba8f38fc6561
lkjy <1443799603@qq.com>
2016-03-01 09:48:30 +0800

    fdsfdsfdsf

--- Heheda/Heheda/ViewController.m
+++ Heheda/Heheda/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"heheda");
+    NSLog(@"dfsdfsdfsdf");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
